77e7eb22d81baf7421136455c2e407dc0213dacf
michaeljacot <michaeljacotguelph@gmail.com>
2020-10-31 12:40:57 -0300

    finalized turning it into a real class, should do the exact same thing now

--- interp.py
+++ interp.py
@@ -18,9 +18,9 @@ class interp:
 
 
     def __init__(self,ticket,domain):
-        sma50,sma200,prices = get50and200(ticket, 4000)
+        sma50,sma200,prices = self.get50and200(ticket, 4000)
         sma50 = sma50[:len(sma200)]
-        result,angles,intersections,polygon_areas,polygons = getAnalysis(sma200,sma50)
+        result,angles,intersections,polygon_areas,polygons,diff = self.getAnalysis(sma200,sma50)
         
         self.ticket = ticket
         self.domain = domain
@@ -32,6 +32,7 @@ class interp:
         self.intersections = intersections
         self.polygon_areas = polygon_areas
         self.polygons = polygons
+        self.diff = diff
         
         
 
@@ -48,8 +49,8 @@ class interp:
         
         prices = getData(ticket, domain)
         
-        sma50 = SMA(prices,50)
-        sma200 = SMA(prices,200)
+        sma50 = self.SMA(prices,50)
+        sma200 = self.SMA(prices,200)
         
         return sma50,sma200,prices
         
@@ -269,18 +270,12 @@ class interp:
             longMa = longMa[:len(shortMa)]
         elif len(longMa) < len(shortMa):
             shortMa = shortMa[:len(longMa)]
-    
-    
+
+        
+        intersections = self.getIntersections(shortMa,longMa)
         
         
-        
-        intersections = getIntersections(shortMa,longMa)
-        
-        #plots the individual intersections points in red on the graph with the data sets
-        for i in intersections:
-            plt.plot(i,longMa[i], marker='o', markersize=3, color="red")
-        
-        lines = getAngles(shortMa,longMa,intersections)
+        lines = self.getAngles(shortMa,longMa,intersections)
         plt.show()
         
         diff = []
@@ -291,7 +286,7 @@ class interp:
         
         
         
-        polygon_areas,polygons = areaAnalysis(shortMa, longMa, intersections)
+        polygon_areas,polygons = self.areaAnalysis(shortMa, longMa, intersections)
     
         
         i = 0
@@ -302,23 +297,13 @@ class interp:
         
         #normalzie both data sets
             
-        diff = normalizeData(diff)
-        polygon_areas = normalizeData(polygon_areas)
+        diff = self.normalizeData(diff)
+        polygon_areas = self.normalizeData(polygon_areas)
         
-        compare = relationship(diff,polygon_areas)
-        
-        angleTitle = "Angle difference: " , len(diff)
-        plt.title(angleTitle)
-        plt.plot(diff)
-        plt.show()
-        polyTitle = "Polygon area: ", len(polygon_areas)
-        plt.title(polyTitle)
-        plt.plot(polygon_areas)
-        plt.show()
-        plt.plot(compare)
+        compare = self.relationship(diff,polygon_areas)
         
         
-        return compare,lines,intersections, polygon_areas,polygons
+        return compare,lines,intersections, polygon_areas,polygons,diff
     
     
     def normalizeData(self, data):
@@ -358,11 +343,30 @@ interpObj = interp(ticket,4000)
 
 
 
+
+
+
 plt.title(ticket)
 plt.plot(interpObj.sma50)
 plt.plot(interpObj.sma200)
+#plots the individual intersections points in red on the graph with the data sets
+for i in interpObj.intersections:
+    plt.plot(i,interpObj.sma50[i], marker='o', markersize=3, color="red")
+plt.show()
+
+angleTitle = "Angle difference: " , len(interpObj.diff) 
+plt.title(angleTitle)
+plt.plot(interpObj.diff)
+plt.show()
+polyTitle = "Polygon area: ", len(interpObj.polygon_areas)
+plt.title(polyTitle)
+plt.plot(interpObj.polygon_areas)
 plt.show()
 plt.plot(interpObj.result)
+plt.title("Interp Index")
+plt.plot(interpObj.result)
+
+
 
 
 
